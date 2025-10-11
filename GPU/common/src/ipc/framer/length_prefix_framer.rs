499696dc6177f436f0c252a1fc6a6688930a9f81
renoseven <dev@renoseven.net>
2025-10-11 09:48:59 +0800

    common: add SPDX license header
    Signed-off-by: renoseven <dev@renoseven.net>

--- GPU/common/src/ipc/framer/length_prefix_framer.rs
+++ GPU/common/src/ipc/framer/length_prefix_framer.rs
@@ -1,7 +1,16 @@
-// src/frame/generic.rs
-
-//! A generic `FrameCodec` implementation with a header containing a magic number,
-//! payload length, and a CRC32 checksum.
+// SPDX-License-Identifier: Mulan PSL v2
+/*
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * This software is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *         http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
 
 use std::ops::{Deref, DerefMut};
 
